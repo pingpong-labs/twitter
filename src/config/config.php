dd5cb8c9878e69716909997103379d2bc1a27681
Gravitano <gravitano16@gmail.com>
2015-04-27 16:13:57 +0700

    remove use url helper

--- src/config/config.php
+++ src/config/config.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-	'consumer_key'		=>	'',
-	'consumer_secret'	=>  '',
-	
-	'oauth_token'		=>	null,
-	'oauth_token_secret'=>  null,
+    'consumer_key'        =>    '',
+    'consumer_secret'    =>  '',
+    
+    'oauth_token'        =>    null,
+    'oauth_token_secret'=>  null,
 
-	'bearer_token'		=>	null,
-	
-	'callback_url'		=>  url('twitter/callback'),
-    'fallback_url'      =>  url('/')
+    'bearer_token'        =>    null,
+    
+    'callback_url'        =>  'twitter/callback',
+    'fallback_url'      =>  '/'
 ];
